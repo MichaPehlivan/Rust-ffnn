8e38a2885ec857460df1f795bdf356cf516efb91
MichaPehlivan <michapehlivan2004@gmail.com>
2022-03-18 18:03:10 +0100

    additional comments

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ type Vector1x2 = SMatrix<f64, 1, 2>;
 type Vector2x1 = SVector<f64, 2>;
 type Vector1x1 = SVector<f64, 1>;
 
+//neural network with 2 input, 2 hidden, and 1 output neuron(s)
 struct Network {
     w1: Matrix2x2,
     b1: Vector1x2,
@@ -16,9 +17,8 @@ struct Network {
     b2: Vector1x1,
 }
 
+//initialize network with random weights and biases 
 fn init() -> Network {
-    println!("initialized network");
-
     Network {
         w1: Matrix2x2::new(random(), random(),
                            random(), random()),
@@ -28,6 +28,7 @@ fn init() -> Network {
     }
 }
 
+//feeds input data forward trough the network and returns the output
 fn feedforward(a0: &Vector1x2, network: &Network) -> Vector1x1 {
     let z1: Vector1x2 = a0 * network.w1 + network.b1;
     let a1: Vector1x2 = z1.map(sigmoid);
@@ -36,6 +37,7 @@ fn feedforward(a0: &Vector1x2, network: &Network) -> Vector1x1 {
     a2
 }
 
+//backpropagation algorithm 
 fn backprop(a0: &Vector1x2, y: &Vector1x1, network: &Network) -> (Matrix2x2, Vector1x2, Vector2x1, Vector1x1) {
     //forward propagation
     let z1: Vector1x2 = a0 * network.w1 + network.b1;
@@ -43,7 +45,7 @@ fn backprop(a0: &Vector1x2, y: &Vector1x1, network: &Network) -> (Matrix2x2, Vec
     let z2: Vector1x1 = a1 * network.w2 + network.b2;
     let a2: Vector1x1 = z2.map(sigmoid);
     
-    //backwards propagation
+    //backward propagation
     let d2: Vector1x1 = -2.0 * (y - a2) * z2.map(sigmoid_prime);
     let dcdw2: Vector2x1 = a1.transpose() * d2;
     let dcdb2: Vector1x1 = d2;
@@ -55,12 +57,14 @@ fn backprop(a0: &Vector1x2, y: &Vector1x1, network: &Network) -> (Matrix2x2, Vec
     (dcdw1, dcdb1, dcdw2, dcdb2)
 }
 
+//trains the network using gradient descent 
 fn train(data: &Matrix4x3, network: &mut Network, learn_rate: f64) {
     let mut generation = 0;
 
     loop {
         generation += 1;
 
+        //initialize gradient vectors 
         let mut dcdw1: Matrix2x2 = Matrix2x2::zeros();
         let mut dcdb1: Vector1x2 = Vector1x2::zeros();
         let mut dcdw2: Vector2x1 = Vector2x1::zeros();
@@ -68,26 +72,30 @@ fn train(data: &Matrix4x3, network: &mut Network, learn_rate: f64) {
 
         let mut avarage_cost = 0.0;
 
+        //loop over training examples 
         for i in 0..data.nrows() {
             let a0: Vector1x2 = data.fixed_slice::<1, 2>(i, 0).into();
             let y: Vector1x1 = data.fixed_slice::<1, 1>(i, 2).into();
             
             let gradient: (Matrix2x2, Vector1x2, Vector2x1, Vector1x1) = backprop(&a0, &y, network);
 
+            //add derivatives of current training example to the avarage gradient
             dcdw1 += gradient.0;
             dcdb1 += gradient.1;
             dcdw2 += gradient.2;
             dcdb2 += gradient.3;
 
+            //compute cost for current training example 
             let a2 = feedforward(&a0, &network);
             let example_cost = cost(&a2, &y);
             avarage_cost += example_cost;
         }
 
+        //avarage cost over all training examples
         avarage_cost /= data.nrows() as f64;
         println!("generation: {}, cost was {}", generation, avarage_cost);
         
-        //update weights and biases
+        //update weights and biases using avarage gradient vector
         let num_of_examples = data.nrows() as f64;
         network.w1 -= learn_rate * dcdw1 / num_of_examples;
         network.b1 -= learn_rate * dcdb1 / num_of_examples;
@@ -96,19 +104,23 @@ fn train(data: &Matrix4x3, network: &mut Network, learn_rate: f64) {
     }
 }
 
+//mean squared error function
 fn cost(a2: &Vector1x1, y: &Vector1x1) -> f64 {
     ((y - a2) * (y - a2))[(0, 0)]
 }
 
+//sigmoid activation function
 fn sigmoid(x: f64) -> f64 {
     1.0 / (1.0 + E.powf(-x))
 }
 
+//derivative of sigmoid activation function
 fn sigmoid_prime(x: f64) -> f64 {
     sigmoid(x) * (1.0 - sigmoid(x))
 }
 
 fn main() {
+    //XOR truth table
     let training_data: Matrix4x3 = Matrix4x3::new(0.0, 0.0, 0.0,
                                                   1.0, 0.0, 1.0,
                                                   0.0, 1.0, 1.0,
